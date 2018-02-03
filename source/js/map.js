0d444a83142dfbd85627456dd7267acc8827d727
Busygina Arina <coolhiya1995@gmail.com>
2018-02-03 20:43:40 +0400

    Добавлен кастомный маркер на карту и создана карта

---
+++ source/js/map.js
@@ -0,0 +1,26 @@
+ymaps.ready(init);
+
+function init() {
+
+  var myMap;
+
+  myMap = new ymaps.Map("map", {
+    center: [59.9387942, 30.3230833],
+    zoom: 17,
+    controls: []
+  });
+  myMap.behaviors.disable('scrollZoom');
+
+  myMap.controls.add("zoomControl", {
+    position: { top: 15, left: 15 }
+  });
+  var myPlacemark = new ymaps.Placemark([59.9387942, 30.3230833], {},
+    {
+      iconLayout: 'default#image',
+      iconImageHref: '/img/icon-map-pin.svg',
+      iconImageSize: [66, 102],
+      iconImageOffset: [-40, -87]
+    });
+
+  myMap.geoObjects.add(myPlacemark);
+}
