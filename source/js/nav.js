94dff377ed1a4a25fad2355635d308d57fd8b45f
Busygina Arina <coolhiya1995@gmail.com>
2018-02-04 19:10:02 +0400

    Рефакторинг кода nav.js

--- source/js/nav.js
+++ source/js/nav.js
@@ -1,23 +1,20 @@
 
-var navMain = document.querySelectorAll(".site-list__item");
+var navMain = Array.from(document.querySelectorAll(".site-list__item"));
 var navToggle = document.querySelector(".main-nav__toggle");
-var newNav = [];
+
+navMain.shift();
+navMain.splice(4, 6)
 for (var i = 0; i < navMain.length; i++) {
-    newNav.push(navMain[i])
-}
-newNav.shift();
-newNav.splice(4, 6)
-for (var i = 0; i < newNav.length; i++) {
-    newNav[i].classList.add("site-list__item--closed");
+  navMain[i].classList.add("site-list__item--closed");
 }
 navToggle.addEventListener("click", function () {
-    for (var i = 0; i < newNav.length; i++) {
-        if (newNav[i].classList.contains("site-list__item--closed")) {
-            newNav[i].classList.remove("site-list__item--closed");
-            newNav[i].classList.add("site-list__item--open");
-        } else {
-            newNav[i].classList.add("site-list__item--closed");
-            newNav[i].classList.remove("site-list__item--open");
-        }
+  for (var i = 0; i < navMain.length; i++) {
+    if (navMain[i].classList.contains("site-list__item--closed")) {
+      navMain[i].classList.remove("site-list__item--closed");
+      navMain[i].classList.add("site-list__item--open");
+    } else {
+      navMain[i].classList.add("site-list__item--closed");
+      navMain[i].classList.remove("site-list__item--open");
     }
+  }
 });
