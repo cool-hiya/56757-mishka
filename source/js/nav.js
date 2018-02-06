50564c242aee118cf8a4b425d407dfaa347f5703
Busygina Arina <coolhiya1995@gmail.com>
2018-02-06 12:54:31 +0400

    Б30 скрытие иконки меню , если нет js

--- source/js/nav.js
+++ source/js/nav.js
@@ -2,11 +2,15 @@
 var navMain = Array.from(document.querySelectorAll(".site-list__item"));
 var navToggle = document.querySelector(".main-nav__toggle");
 
+navToggle.classList.remove("main-nav__toggle--no-js");
+
 navMain.shift();
-navMain.splice(4, 6)
+navMain.splice(4, 6);
+
 for (var i = 0; i < navMain.length; i++) {
   navMain[i].classList.add("site-list__item--closed");
 }
+
 navToggle.addEventListener("click", function () {
   navToggle.classList.toggle("main-nav__toggle--closed");
   for (var i = 0; i < navMain.length; i++) {
