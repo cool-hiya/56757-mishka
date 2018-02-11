90b8a8406f559b4fa3f78c13fa653964c3eec440
Busygina Arina <coolhiya1995@gmail.com>
2018-02-11 13:45:25 +0400

    Исправлен скрипт модалки для IE

--- source/js/modal.js
+++ source/js/modal.js
@@ -2,11 +2,11 @@ var modal = document.querySelector(".modal");
 var modalButton = document.querySelector(".modal__button");
 var modalOpener = document.querySelectorAll(".js-modal-opener");
 
-modalOpener.forEach(function (modalOpener) {
-  modalOpener.addEventListener("click", function () {
+for (var i = 0; i < modalOpener.length; i++) {
+  modalOpener[i].addEventListener("click", function () {
     modal.classList.add("modal--visible");
   });
-});
+}
 
 modalButton.addEventListener("click", function () {
   modal.classList.remove("modal--visible");
